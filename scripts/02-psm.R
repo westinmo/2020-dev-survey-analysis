013d65a168662efb88f1122b37fc5c52f110fc1b
westinmo <morgaine.westin@gmail.com>
2021-03-30 22:29:32 -0400

    Fixed PSM issue

--- scripts/02-psm.R
+++ scripts/02-psm.R
@@ -1,39 +1,44 @@
 library(arm)
-
+survey_test <- survey_unnest
 #Factorizing gender
-survey_unnest$Gender <- as.factor(survey_unnest$Gender)
+survey_test$Gender <- as.factor(survey_test$Gender)
 
 #Removing NAs from categories used for PSM
-survey_unnest <- survey_unnest [!is.na(survey_unnest$EdLevel), ]
-survey_unnest <- survey_unnest [!is.na(survey_unnest$YearsCodePro), ] 
-survey_unnest <- survey_unnest [!is.na(survey_unnest$DevType), ]
-survey_unnest <- survey_unnest [!is.na(survey_unnest$Age), ]
+survey_test<- survey_test[!is.na(survey_test$EdLevel), ]
+survey_test <- survey_test[!is.na(survey_test$YearsCodePro), ] 
+survey_test <- survey_test[!is.na(survey_test$DevType), ]
+survey_test <- survey_test[!is.na(survey_test$Age), ]
 
 #PSM for gender
 propensity_score <- glm(Gender ~ Ethnicity + EdLevel + Ethnicity + DevType + Age + YearsCodePro,
                         family = binomial,
-                        data = survey_unnest)
-summary(propensity_score)
-survey_unnest <- 
+                        data = survey_test)
+
+survey_test <- 
   augment(propensity_score, 
-          data = survey_unnest,
+          data = survey_test,
           type.predict = "response") %>% 
   dplyr::select(-.resid, -.std.resid, -.hat, -.sigma, -.cooksd) 
 
-survey_unnest <- 
-  survey_unnest %>% 
+survey_test <- 
+  survey_test %>% 
   arrange(.fitted, Gender)
 
-survey_unnest$Gender2 <- revalue(survey_unnest$Gender, c("Man"=1, "Woman"=2, "Non-binary, genderqueer, or gender non-conforming"=3))
-survey_unnest$Gender2 <- as.integer(survey_unnest$Gender2)
+survey_test$Gender2 <- revalue(survey_test$Gender, c("Man"=0, "Woman"=1, "Non-binary, genderqueer, or gender non-conforming"=2))
+survey_test$Gender2 <- as.integer(as.character(survey_test$Gender2))
+summary(survey_test$Gender2)
 
+
+summary(survey_test)
 #Matching
-matches <- arm::matching(z = survey_unnest$Gender2, score = survey_unnest$.fitted)
-survey_unnest <- cbind(survey_unnest, matches)
-
-table(survey_unnest$Gender)
+matches <- arm::matching(z = survey_test$Gender2, score = survey_test$.fitted, replace = F)
+survey_test <- cbind(survey_test, matches)
 
 survey_matched <- 
-  survey_unnest %>% 
-  filter(match.ind != 1) %>% 
+  survey_test %>% 
+  filter(match.ind != 0) %>% 
   dplyr::select(-match.ind, -pairs, -Gender2)
+
+table(survey_matched$Gender)
+
+??match.ind
